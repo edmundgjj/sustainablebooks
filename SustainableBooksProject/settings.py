9a8e442155758598dc19b9bb4f91471d6cc04c21
edmundgjj <edmundgjj@gmail.com>
2020-12-22 10:56:39 +0000

    Added Books App and added index template for books

--- SustainableBooksProject/settings.py
+++ SustainableBooksProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'b&ff_%9tks+wxjj(v5pn=do7%%3a8=677ew*r8c=xin2)sx%e+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #my apps
+    # my apps
     'books'
 ]
 
--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Books app")
+    return render(request,'books/index.template.html')
