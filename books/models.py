670c1e7a382ea39fedffa5908b294afaaa2e64c8
edmundgjj <edmundgjj@gmail.com>
2020-12-23 12:26:14 +0000

    Added Genre to books model

--- books/models.py
+++ books/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
 class Book(models.Model):
     title = models.CharField(blank=False, max_length=255)
     author = models.CharField(blank=False, max_length=255)
     version = models.CharField(blank=False, max_length=255)
     desc = models.TextField(blank=False)
     price = models.FloatField(blank=False)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title 
+        return self.title
+
 
 class Author(models.Model):
     first_name = models.CharField(blank=False, max_length=255)
@@ -17,4 +28,4 @@ class Author(models.Model):
     date_of_birth = models.DateField(blank=False)
 
     def __str__(self):
-        return self.title 
+        return self.title
