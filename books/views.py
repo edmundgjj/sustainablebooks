d40675e8611b39473d60adfd8181ec37458b0aac
edmundgjj <edmundgjj@gmail.com>
2020-12-23 12:17:57 +0000

    Allow users to update the book information

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
 from .models import Book
 from .forms import BookForm
 
@@ -12,7 +12,37 @@ def index(request):
     })
 
 def create_book(request):
-    create_form = BookForm()
-    return render(request, 'books/createbook.template.html', {
-        'form':create_form
-    })
+    if request.method == 'POST': 
+        create_form = BookForm(request.POST)
+
+        if create_form.is_valid():
+            create_form.save()
+            return redirect(reverse(index))
+        else:
+            return render(request, 'books/createbook.template.html', {
+                'form': create_form
+            })
+    else:
+        create_form = BookForm()
+        return render(request, 'books/createbook.template.html', {
+            'form': create_form
+        })
+
+def update_book(request, book_id):
+    book_being_updated = get_object_or_404(Book, pk=book_id)
+
+    if request.method == "POST":
+
+        book_form = BookForm(request.POST, instance=book_being_updated)
+        if book_form.is_valid():
+            book_form.save()
+            return redirect(reverse(index))
+        else:
+            return render(request, 'books/updatebook.template.html', {
+                "form": book_form
+            })
+    else:
+        book_form = BookForm(instance=book_being_updated)
+        return render(request, 'books/updatebook.template.html', {
+            "form": book_form
+        })
